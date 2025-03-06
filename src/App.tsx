381874ab1fe57fa0c4dfdad6fb7afa2c002d9be2
arpan-mondal-3000 <arpan954mondal@gmail.com>
2025-03-06 10:44:46 +0530

    initial commit with tailwind and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+      <div className="text-blue-500 text-3xl">Driver management system</div>
+      <Button>Signup/Login</Button>
+    </>
+  )
+}
+
+export default App
